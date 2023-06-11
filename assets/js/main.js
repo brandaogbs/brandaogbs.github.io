48306b809876d844cb2dd9242d84cd0a5b38c6d9
brandaogbs <brandaogbs@gmail.com>
2023-06-11 18:05:14 -0300

    fix: logo live mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,38 @@
 (() => {
-  // Theme switch
-  const body = document.body;
-  const lamp = document.getElementById("mode");
+    // Theme switch
+    const body = document.body;
+    const lamp = document.getElementById("mode");
 
-  const toggleTheme = (state) => {
-    if (state === "dark") {
-      localStorage.setItem("theme", "light");
-      body.removeAttribute("data-theme");
-    } else if (state === "light") {
-      localStorage.setItem("theme", "dark");
-      body.setAttribute("data-theme", "dark");
-    } else {
-      initTheme(state);
+    const toggleTheme = (state) => {
+        if (state === "dark") {
+            localStorage.setItem("theme", "light");
+            body.removeAttribute("data-theme");
+        } else if (state === "light") {
+            localStorage.setItem("theme", "dark");
+            body.setAttribute("data-theme", "dark");
+        } else {
+            initTheme(state);
+        }
+
+        if (window.location.pathname === "/") {
+            document.getElementById("home-logo").src = `/assets/img/logo-${localStorage.getItem('theme')}.png`;
+        }
+    };
+
+    lamp.addEventListener("click", () =>
+        toggleTheme(localStorage.getItem("theme"))
+    );
+
+    // Blur the content when the menu is open
+    const cbox = document.getElementById("menu-trigger");
+    if (window.location.pathname === "/") {
+        document.getElementById("home-logo").src = `/assets/img/logo-${localStorage.getItem('theme')}.png`;
     }
-  };
 
-  lamp.addEventListener("click", () =>
-    toggleTheme(localStorage.getItem("theme"))
-  );
-
-  // Blur the content when the menu is open
-  const cbox = document.getElementById("menu-trigger");
-
-  cbox.addEventListener("change", function () {
-    const area = document.querySelector(".wrapper");
-    this.checked
-      ? area.classList.add("blurry")
-      : area.classList.remove("blurry");
-  });
+    cbox.addEventListener("change", function() {
+        const area = document.querySelector(".wrapper");
+        this.checked
+            ? area.classList.add("blurry")
+            : area.classList.remove("blurry");
+    });
 })();
